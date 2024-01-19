e98a009606771be33cb6de257870629a7f3d6408
xMortal <emanuelborges13998@gmail.com>
2024-01-19 11:14:47 +0000

    ola

--- bingo_prof.cpp
+++ bingo_prof.cpp
@@ -1,16 +1,28 @@
 /**
 * @file duvidas.cpp
 * @author Emanuel Borges
-* @brief Jogo bingo, cohecimento
+* @brief Jogo bingo, conhecimento
 * @version 0.1
 * @date 17-01-2024
 */
+
+// int : 4 bytes (32 bites)  00000000 00000000 00000000 00000000
+//char : 1 byte (8 bites)    00000000
+//bool : 1 byte (8 bites)    00000000(false) 00000001 (true) 
+//float : 4 bytes (32 bites) 00000000 00000000 00000000 00000000
+//double : 8 bytes (64 bites)
+//double : 8 bytes (64b bites)
 
 #include <iostream>
 #include <iomanip>
 #include <cstdlib>
 #include <ctime>
-#include <unistd.h>
+
+#ifdef _WIN32
+    #include <windows.h>
+#else
+    #include <unistd.h>
+#endif
 
 using namespace std;
 
@@ -26,57 +38,53 @@ int anterior         = 0;
 
 int numeroDeBolas, tipoSorteio, numeroDeCartoes;
 
-string red      = "\033[1;31m";
-string green    = "\033[1;32m"; 
-string blue     = "\033[1;34m";
-string reset    = "\033[0m";
-string bold     = "\033[1m";
+string red       = "\033[1;31m";
+string green     = "\033[1;32m"; 
+string blue      = "\033[1;34m";
+string reset     = "\033[0m";
+string bold      = "\033[1m";
+string blink     = "\033[5m";
+
+string figletFont = "big";
 
 typedef struct {
     int numero;
     bool sorteado;
 } bingoPanel;
 
-bingoPanel bingo [] = {};
+bingoPanel bingo[100];  
 
 /**
-* @brief Função para contar uma string no ecrã
+* @brief Função para centralizar uma string no console
 *
 * @param mensagem
 */
-
-
-void centerString(string manesagem) {
-    int l = mensagem.lengh();
-    int pos = (int)(numColunasPainel - 3 (l)) / 2);
+void centerString(string mensagem) {
+    int l = mensagem.length();
+    int pos = (int)((numColunasPainel - 3 * l) / 2);
     for (int i = 0; i < pos; i++) {
-        cout << "";
+        cout << " ";
     }
-    cout << blue;
-    cout << mensagem << reset;
-    cout << blue << blue;
-    }
+    cout << blue << mensagem << reset << endl;
+}
+
 /**
-* 
-* @brief 
+* @brief Função para inicializar o array de bingo
 */
-void intArray() {
-    for (int i = 0; i <= numDeBolas; i++) {
-        bing[1].numero = i + 1;
-        bingo[1].sorteado = false;
+void initArray() {
+    for (int i = 0; i < numeroDeBolas; i++) {
+        bingo[i].numero = i + 1;
+        bingo[i].sorteado = false;
     }
 }
 
 /**
 * @brief Função de apresentação dos cartões
-* 
 */
 void opcoes() {
-
     tipoSorteio = 0;
 
     while (tipoSorteio < 1 || tipoSorteio > 4) {
-
         system("clear || cls");
         cout << blue
             << "--------------------------------\n"
@@ -92,20 +100,19 @@ void opcoes() {
         cin >> tipoSorteio;
     }
 
-    if (tipoSorteio != 1 && tipoSorteio != )
-
+    if (tipoSorteio != 1 && tipoSorteio != 2 && tipoSorteio != 3 && tipoSorteio != 4) {
+        
+    }
 }
 
 /*
-* @brif Função para apresentação do painel de bingo
+* @brief Função para apresentação do painel de bingo
 *
 * @param sorteado
 * @param sorteadoAnteriormente
 */
-
 void showBingoPanel(int sorteado, int sorteadoAnteriormente) {
-
-    system("clear ||  cls");
-
+    system("clear || cls");
     centerString("J O G O  D O  B I N G O");
-
+ 
+}
